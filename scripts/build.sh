6de39af54f419f70fe92784021b5170523565dee
nantokathium <191812865+nantokathium@users.noreply.github.com>
2025-02-04 21:57:37 +0900

    qujisho.txt内でコメントできるようにした。pushのたびに成果物を保存するようにした

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,13 @@
-mkdir release
-cp qujisho.txt release/qujisho-general.txt
-cat qujisho.txt | perl -wpe 's/\n/\r\n/g' | ( printf "\xff\xfe" ; iconv -f utf-8 -t utf-16le )   > release/qujisho-windows.txt
+set -euo pipefail
+
+SRC=./qujisho.txt
+BUILD_DIR="./build/"
+TRIM_SRC=./build/qujisho.trim.txt
+RELEASE_GENERAL=${BUILD_DIR}/qujisho-general.txt
+RELEASE_WIN=${BUILD_DIR}/qujisho-windows.txt
+
+# コメント行を取り除く。
+cat $SRC | perl -wpe 's/^#.*?$//g' > $TRIM_SRC
+
+cp $TRIM_SRC $RELEASE_GENERAL
+cat $TRIM_SRC | perl -wpe 's/\n/\r\n/g' | ( printf "\xff\xfe" ; iconv -f utf-8 -t utf-16le )   > $RELEASE_WIN
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,1 @@
-cat release/qujisho-general.txt
-iconv -f UTF-16 -t UTF-8 release/qujisho-windows.txt
+echo no-op
